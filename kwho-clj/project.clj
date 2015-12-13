8441a9285c1334292f586e5e7274150219644015
Jacob Morzinski <jmorzins@mit.edu>
2015-12-13 01:19:18 -0500

    Use lein-bin to generate a jar-packed .bat file.
    Currently uses a personal fork of lein-bin installed
    with "lein install".

--- kwho-clj/project.clj
+++ kwho-clj/project.clj
@@ -1,9 +1,12 @@
-(defproject kwho "0.1.0-SNAPSHOT"
+(defproject kwho "0.1.0"
   :description "FIXME: write description"
   :url "https://github.com/jacobmorzinski/kwho"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[lein-bin "0.3.6-SNAPSHOT"]] #_ "https://github.com/jacobmorzinski/lein-bin"
+  :bin {:name "kwho.bat"} #_ "I wish I could use a conditional..."
   :main ^:skip-aot kwho.core
   :target-path "target/%s"
+  :jar-exclusions [#"(^|\\|/)[._](.*\.|)s[a-w][a-z]$"] #_ "vim .swp files"
   :profiles {:uberjar {:aot :all}})
