9c9aab58aabe2ca9b0d1fe798aba0eb912f8ecd4
Jacob Morzinski <jmorzins@mit.edu>
2016-01-10 12:18:00 -0500

    Cleanup and reorganize

--- kwho-rust/src/main.rs
+++ kwho-rust/src/main.rs
@@ -3,6 +3,18 @@
 // https://github.com/rust-lang/getopts
 extern crate getopts;
 
+// https://doc.rust-lang.org/stable/book/ffi.html
+extern crate libc;
+
+// Rust/C bindings for:
+// krb5_init_context
+// krb5_cc_default
+// krb5_cc_get_principal
+// com_err
+#[allow(non_camel_case_types)]
+mod krb5;
+mod com_err;
+
 use getopts::Options;
 use std::env;
 use std::ffi::{CStr,CString};
@@ -10,79 +22,6 @@ use std::io::{self,Write};
 use std::process;
 use std::ptr;
 
-// https://doc.rust-lang.org/stable/book/ffi.html
-extern crate libc;
-
-#[allow(dead_code, non_camel_case_types)]
-pub mod com_err {
-    #[link(name = "krb5")]
-    #[link(name = "k5crypto")]
-    #[link(name = "com_err")]
-    extern "C" {
-        pub fn com_err(arg1: *const ::libc::c_char, arg2: ::libc::c_int,
-                       arg3: *const ::libc::c_char, ...) -> ();
-    }
-}
-
-#[allow(dead_code, non_camel_case_types)]
-pub mod krb5 {
-    pub type krb5_int32 = ::libc::c_int;
-    pub type krb5_error_code = krb5_int32;
-    pub type krb5_magic = krb5_error_code;
-
-    pub enum Struct__krb5_context { }
-    pub type krb5_context = *mut Struct__krb5_context;
-    #[link(name = "krb5")]
-    #[link(name = "k5crypto")]
-    #[link(name = "com_err")]
-    extern "C" {
-        pub fn krb5_init_context(context: *mut krb5_context)
-          -> krb5_error_code;
-        pub fn krb5_cc_default(context: krb5_context, ccache: *mut krb5_ccache)
-          -> krb5_error_code;
-        pub fn krb5_cc_get_principal(context: krb5_context, cache: krb5_ccache,
-                                     principal: *mut krb5_principal)
-          -> krb5_error_code;
-
-    }
-
-    pub enum Struct__krb5_ccache { }
-    pub type krb5_ccache = *mut Struct__krb5_ccache;
-
-    #[repr(C)]
-    #[derive(Copy)]
-    pub struct Struct__krb5_data {
-        pub magic: krb5_magic,
-        pub length: ::libc::c_uint,
-        pub data: *mut ::libc::c_char,
-    }
-    impl ::std::clone::Clone for Struct__krb5_data {
-        fn clone(&self) -> Self { *self }
-    }
-    impl ::std::default::Default for Struct__krb5_data {
-        fn default() -> Self { unsafe { ::std::mem::zeroed() } }
-    }
-    pub type krb5_data = Struct__krb5_data;
-
-    #[repr(C)]
-    #[derive(Copy)]
-    pub struct Struct_krb5_principal_data {
-        pub magic: krb5_magic,
-        pub realm: krb5_data,
-        pub data: *mut krb5_data,
-        pub length: krb5_int32,
-        pub _type: krb5_int32,
-    }
-    impl ::std::clone::Clone for Struct_krb5_principal_data {
-        fn clone(&self) -> Self { *self }
-    }
-    impl ::std::default::Default for Struct_krb5_principal_data {
-        fn default() -> Self { unsafe { ::std::mem::zeroed() } }
-    }
-    pub type krb5_principal_data = Struct_krb5_principal_data;
-    pub type krb5_principal = *mut krb5_principal_data;
-
-}
 
 // This is a silly amount of work, but I wanted to learn how to
 // get the progname into a global variable.
@@ -110,14 +49,13 @@ fn usage(opts: &Options) -> ! {
 }
 
 fn main() {
-// Debugging code:
-//    println!("Progname is {}", *PROGNAME);
 
-    let progname = (*PROGNAME).clone();
+    let progname = CString::new((*PROGNAME).clone())
+        .unwrap()
+        .into_raw();
 
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
-    opts.optopt("c", "", "specifies credential cache", "CCNAME");
     opts.optflag("h", "help", "print this help menu");
 
     let matches = match opts.parse(&args[1..]) {
@@ -130,18 +68,12 @@ fn main() {
         usage(&opts);
     }
     
-    let ccache: Option<String> = matches.opt_str("c");
-// Debugging code:
-    if ccache.is_some() {
-        // println!("Sorry, IGNORING requested ccache ({})", ccache.clone().unwrap());
-    }
-
     let mut ctx: krb5::krb5_context = ptr::null_mut();
     let mut code: krb5::krb5_error_code = unsafe { krb5::krb5_init_context(&mut ctx) };
     if code != 0 {
         unsafe {
-            com_err::com_err(CString::new(progname).unwrap().into_raw(),
-                             code,
+            com_err::com_err(progname,
+                             code as i64,
                              CString::new("while initializing krb5").unwrap().into_raw());
         }
         std::process::exit(1);
@@ -151,8 +83,8 @@ fn main() {
     code = unsafe {krb5::krb5_cc_default(ctx, &mut cache)};
     if code != 0 {
         unsafe {
-            com_err::com_err(CString::new(progname).unwrap().into_raw(),
-                             code,
+            com_err::com_err(progname,
+                             code as i64,
                              CString::new("while getting default ccache").unwrap().into_raw());
         }
         std::process::exit(1);
@@ -162,8 +94,8 @@ fn main() {
     code = unsafe { krb5::krb5_cc_get_principal(ctx, cache, &mut princ) };
     if code != 0 {
         unsafe {
-            com_err::com_err(CString::new(progname).unwrap().into_raw(),
-                             code,
+            com_err::com_err(progname,
+                             code as i64,
                              CString::new("while retrieving principal name").unwrap().into_raw());
         }
         std::process::exit(1);
