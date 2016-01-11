d935b5beceab39fcd1422958f8448b714631eea5
Jacob Morzinski <jmorzins@mit.edu>
2016-01-10 19:09:23 -0500

    Cleanup, and exploring encapsulation.

--- kwho-rust/src/main.rs
+++ kwho-rust/src/main.rs
@@ -6,12 +6,11 @@ extern crate getopts;
 // https://doc.rust-lang.org/stable/book/ffi.html
 extern crate libc;
 
-// Rust/C bindings for:
+// Rust FFI for:
 // krb5_init_context
 // krb5_cc_default
 // krb5_cc_get_principal
 // com_err
-#[allow(non_camel_case_types)]
 mod krb5;
 mod com_err;
 
@@ -22,6 +21,26 @@ use std::io::{self,Write};
 use std::process;
 use std::ptr;
 
+pub struct Krb5Context {
+    ctx: krb5::krb5_context,
+}
+impl Krb5Context {
+    pub fn new() -> Option<Krb5Context> {
+        let mut ctx: krb5::krb5_context = ptr::null_mut();
+        unsafe {
+            let code = krb5::krb5_init_context(&mut ctx);
+            if code != 0 {
+                let whoami = CString::new((*PROGNAME).clone())
+                    .unwrap().into_raw();
+                let detail = CString::new("while initializing krb5").
+                    unwrap().into_raw();
+                com_err::com_err(whoami, code as i64, detail);
+                return None;
+            }
+        }
+        Some(Krb5Context { ctx: ctx, })
+    }
+}
 
 // This is a silly amount of work, but I wanted to learn how to
 // get the progname into a global variable.
@@ -68,16 +87,9 @@ fn main() {
         usage(&opts);
     }
     
-    let mut ctx: krb5::krb5_context = ptr::null_mut();
-    let mut code: krb5::krb5_error_code = unsafe { krb5::krb5_init_context(&mut ctx) };
-    if code != 0 {
-        unsafe {
-            com_err::com_err(progname,
-                             code as i64,
-                             CString::new("while initializing krb5").unwrap().into_raw());
-        }
-        std::process::exit(1);
-    }
+    let mut code: krb5::krb5_error_code;
+
+    let ctx = Krb5Context::new().unwrap().ctx;
 
     let mut cache: krb5::krb5_ccache = ptr::null_mut();
     code = unsafe {krb5::krb5_cc_default(ctx, &mut cache)};
